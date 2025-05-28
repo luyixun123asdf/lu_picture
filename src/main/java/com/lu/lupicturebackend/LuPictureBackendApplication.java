88828716952899ebfdc4d0e29a5c8662797fae63
lyx <hello@lyx.cn>
2025-05-28 15:50:18 +0800

    项目从初始化

---
+++ src/main/java/com/lu/lupicturebackend/LuPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lu.lupicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuPictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lu/lupicturebackend/LuPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lu.lupicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
