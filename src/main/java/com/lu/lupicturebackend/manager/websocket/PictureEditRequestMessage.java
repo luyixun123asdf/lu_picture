316ff63b338ff80958b2957a0b4317b17e7d8637
lyx <hello@lyx.cn>
2025-06-14 15:58:56 +0800

    实现多人协作功能，定义请求类、相应类、以及操作枚举等

---
+++ src/main/java/com/lu/lupicturebackend/manager/websocket/PictureEditRequestMessage.java
@@ -0,0 +1,25 @@
+package com.lu.lupicturebackend.manager.websocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+/**
+ * 图片编辑请求消息，也就是前端要发送给后端的参数
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PictureEditRequestMessage {
+
+    /**
+     * 消息类型，例如 "ENTER_EDIT", "EXIT_EDIT", "EDIT_ACTION"
+     */
+    private String type;
+
+    /**
+     * 执行的编辑动作
+     */
+    private String editAction;
+}
